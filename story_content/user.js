d517c9bedf9e1daf82deb1d513089bc447dd0776
April-September <33736567+April-September@users.noreply.github.com>
2019-06-01 17:18:53 +0200

    test
    Test

--- story_content/user.js
+++ story_content/user.js
@@ -2,13 +2,69 @@ function ExecuteScript(strId)
 {
   switch (strId)
   {
-      case "6Z7U5Xj8aUQ":
+      case "5iikW3cLeSJ":
         Script1();
+        break;
+      case "6XCL81ST6mU":
+        Script2();
+        break;
+      case "5Zm4lEkZuUV":
+        Script3();
+        break;
+      case "5WBf7t3zzMc":
+        Script4();
+        break;
+      case "68rtGgedEr6":
+        Script5();
+        break;
+      case "6730qI4C4Ge":
+        Script6();
+        break;
+      case "5s3fT8k0Ms6":
+        Script7();
+        break;
+      case "6M9RBb7UZyz":
+        Script8();
         break;
   }
 }
 
 function Script1()
+{
+  window.print()
+}
+
+function Script2()
+{
+  window.print()
+}
+
+function Script3()
+{
+  window.print()
+}
+
+function Script4()
+{
+  window.print()
+}
+
+function Script5()
+{
+  window.print()
+}
+
+function Script6()
+{
+  window.print()
+}
+
+function Script7()
+{
+  window.print()
+}
+
+function Script8()
 {
   window.print()
 }
