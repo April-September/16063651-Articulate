0fc48e6093de20e375b236703285af72633ff249
April-September <33736567+April-September@users.noreply.github.com>
2019-08-19 16:43:41 +0200

    Final publication (16:43 German Time)

---
+++ story_content/user.js
@@ -0,0 +1,553 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "6PoNnhTKnEo":
+        Script1();
+        break;
+      case "65KPx5SN9rT":
+        Script2();
+        break;
+      case "6BpIb7iVySz":
+        Script3();
+        break;
+      case "5qRZQYUHI3H":
+        Script4();
+        break;
+      case "5WP3mcBRYJB":
+        Script5();
+        break;
+      case "6ljfeYnKaP0":
+        Script6();
+        break;
+      case "5oOsxUTNmdw":
+        Script7();
+        break;
+      case "6oTCPEHIEt7":
+        Script8();
+        break;
+      case "5jG5R7g8JoR":
+        Script9();
+        break;
+      case "64rLBQb6lGw":
+        Script10();
+        break;
+      case "5ol4ok1h4IO":
+        Script11();
+        break;
+      case "6j0sLNCDdcg":
+        Script12();
+        break;
+      case "5aPd2llWL1k":
+        Script13();
+        break;
+      case "5igDanHUWy4":
+        Script14();
+        break;
+      case "5kymZxMvB66":
+        Script15();
+        break;
+      case "5dtFe4msf7z":
+        Script16();
+        break;
+      case "6iGagN5aUTP":
+        Script17();
+        break;
+      case "5g8vghfQSIQ":
+        Script18();
+        break;
+      case "62n33xnaVVP":
+        Script19();
+        break;
+      case "6lfldphcwOY":
+        Script20();
+        break;
+      case "5VOE6o3tzto":
+        Script21();
+        break;
+      case "5ePEzXt1xed":
+        Script22();
+        break;
+      case "6AG3nHP0ICA":
+        Script23();
+        break;
+      case "6dx7i6hyzb4":
+        Script24();
+        break;
+      case "6LPnMyDzWkW":
+        Script25();
+        break;
+      case "6OYaEq5xJIc":
+        Script26();
+        break;
+      case "66w1P8hH15B":
+        Script27();
+        break;
+      case "5vXocIkCkfV":
+        Script28();
+        break;
+      case "5XzsbYMrl4a":
+        Script29();
+        break;
+      case "5t7SvjhdZNb":
+        Script30();
+        break;
+      case "5tBx4Gs88ha":
+        Script31();
+        break;
+      case "6foQXmJX7TA":
+        Script32();
+        break;
+      case "6UAbCMTJLCI":
+        Script33();
+        break;
+      case "689AWPXD17l":
+        Script34();
+        break;
+      case "6bf7h9APgEg":
+        Script35();
+        break;
+      case "5aLMXprpXnD":
+        Script36();
+        break;
+      case "6lCA2BtgPgq":
+        Script37();
+        break;
+      case "6RqB2teHeCM":
+        Script38();
+        break;
+      case "5l6jCh8mvCE":
+        Script39();
+        break;
+      case "5jaUzUOT8si":
+        Script40();
+        break;
+      case "6QgvpKjCa6f":
+        Script41();
+        break;
+      case "6maRgSrdBAA":
+        Script42();
+        break;
+      case "6qoazJV6Idq":
+        Script43();
+        break;
+      case "6WQJgvPm9lN":
+        Script44();
+        break;
+      case "5yBdx5eoGLN":
+        Script45();
+        break;
+      case "6dW7nImYEuk":
+        Script46();
+        break;
+      case "6UfU9bKVgxH":
+        Script47();
+        break;
+      case "6co4l3ogyJP":
+        Script48();
+        break;
+      case "6XSHWbeBzj7":
+        Script49();
+        break;
+      case "67H3LliwKIm":
+        Script50();
+        break;
+      case "5du0geJWXzC":
+        Script51();
+        break;
+      case "6lItQEgiUhK":
+        Script52();
+        break;
+      case "64lAnbrR7Z6":
+        Script53();
+        break;
+      case "6WlFm3vajXF":
+        Script54();
+        break;
+      case "63uOUuxT1Yd":
+        Script55();
+        break;
+      case "6DhuhSOkePk":
+        Script56();
+        break;
+      case "5sNfZUnlzBt":
+        Script57();
+        break;
+      case "6itkTmV7bH0":
+        Script58();
+        break;
+      case "6SYTWStPEdp":
+        Script59();
+        break;
+      case "5bvdmB0jp9r":
+        Script60();
+        break;
+      case "6kvEqDBqSls":
+        Script61();
+        break;
+      case "5uyT1nBXheb":
+        Script62();
+        break;
+      case "6IXN6ZsfpWv":
+        Script63();
+        break;
+      case "5w6hVPsIMcy":
+        Script64();
+        break;
+      case "6Vql2ZJHfmM":
+        Script65();
+        break;
+      case "5z7XayA2lvY":
+        Script66();
+        break;
+  }
+}
+
+function Script1()
+{
+  window.print()
+}
+
+function Script2()
+{
+  window.print()
+}
+
+function Script3()
+{
+  window.print()
+}
+
+function Script4()
+{
+  window.print()
+}
+
+function Script5()
+{
+  var currentTime = new Date()
+var month = currentTime.getMonth() + 1
+var day = currentTime.getDate()
+var year = currentTime.getFullYear()
+var dateString=day + "/" + month + "/" + year
+var player = GetPlayer();
+player.SetVar("FNNSystemDate",dateString);
+}
+
+function Script6()
+{
+  window.print()
+}
+
+function Script7()
+{
+  window.print()
+}
+
+function Script8()
+{
+  window.print()
+}
+
+function Script9()
+{
+  window.print()
+}
+
+function Script10()
+{
+  window.print()
+}
+
+function Script11()
+{
+  window.print()
+}
+
+function Script12()
+{
+  var currentTime = new Date()
+var month = currentTime.getMonth() + 1
+var day = currentTime.getDate()
+var year = currentTime.getFullYear()
+var dateString=day + "/" + month + "/" + year
+var player = GetPlayer();
+player.SetVar("FNNSystemDate",dateString);
+}
+
+function Script13()
+{
+  window.print()
+}
+
+function Script14()
+{
+  window.print()
+}
+
+function Script15()
+{
+  window.print()
+}
+
+function Script16()
+{
+  window.print()
+}
+
+function Script17()
+{
+  window.print()
+}
+
+function Script18()
+{
+  window.print()
+}
+
+function Script19()
+{
+  window.print()
+}
+
+function Script20()
+{
+  window.print()
+}
+
+function Script21()
+{
+  window.print()
+}
+
+function Script22()
+{
+  window.print()
+}
+
+function Script23()
+{
+  window.print()
+}
+
+function Script24()
+{
+  window.print()
+}
+
+function Script25()
+{
+  window.print()
+}
+
+function Script26()
+{
+  window.print()
+}
+
+function Script27()
+{
+  window.print()
+}
+
+function Script28()
+{
+  window.print()
+}
+
+function Script29()
+{
+  window.print()
+}
+
+function Script30()
+{
+  window.print()
+}
+
+function Script31()
+{
+  window.print()
+}
+
+function Script32()
+{
+  window.print()
+}
+
+function Script33()
+{
+  window.print()
+}
+
+function Script34()
+{
+  window.print()
+}
+
+function Script35()
+{
+  window.print()
+}
+
+function Script36()
+{
+  window.print()
+}
+
+function Script37()
+{
+  window.print()
+}
+
+function Script38()
+{
+  window.print()
+}
+
+function Script39()
+{
+  window.print()
+}
+
+function Script40()
+{
+  window.print()
+}
+
+function Script41()
+{
+  window.print()
+}
+
+function Script42()
+{
+  window.print()
+}
+
+function Script43()
+{
+  window.print()
+}
+
+function Script44()
+{
+  window.print()
+}
+
+function Script45()
+{
+  window.print()
+}
+
+function Script46()
+{
+  window.print()
+}
+
+function Script47()
+{
+  window.print()
+}
+
+function Script48()
+{
+  window.print()
+}
+
+function Script49()
+{
+  window.print()
+}
+
+function Script50()
+{
+  window.print()
+}
+
+function Script51()
+{
+  window.print()
+}
+
+function Script52()
+{
+  window.print()
+}
+
+function Script53()
+{
+  window.print()
+}
+
+function Script54()
+{
+  window.print()
+}
+
+function Script55()
+{
+  window.print()
+}
+
+function Script56()
+{
+  window.print()
+}
+
+function Script57()
+{
+  window.print()
+}
+
+function Script58()
+{
+  var currentTime = new Date()
+var month = currentTime.getMonth() + 1
+var day = currentTime.getDate()
+var year = currentTime.getFullYear()
+var dateString=day + "/" + month + "/" + year
+var player = GetPlayer();
+player.SetVar("FNNSystemDate",dateString);
+}
+
+function Script59()
+{
+  window.print()
+}
+
+function Script60()
+{
+  window.print()
+}
+
+function Script61()
+{
+  window.print()
+}
+
+function Script62()
+{
+  window.print()
+}
+
+function Script63()
+{
+  window.print()
+}
+
+function Script64()
+{
+  window.print()
+}
+
+function Script65()
+{
+  window.print()
+}
+
+function Script66()
+{
+  window.print()
+}
+
